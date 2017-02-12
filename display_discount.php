0a3b33fbd2b8726e3943af6d06bf62ed1ac32d88
Srinidhi Krishnaswamy <sk2423@afsconnect1.njit.edu>
2017-02-12 16:41:40 -0500

    included all the changes

--- display_discount.php
+++ display_discount.php
@@ -24,24 +24,26 @@
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-    <main>
-        <h1>|DISPLAY RESULTS|</h1>
+    <main><h1>Product Discount Calculator</h1>
 
-        <label>Product Description:</label>
-	        <span><?php echo ''; ?></span><br>
+            <label>Product Description:</label>
+	            <span><?php echo $product_description_escaped; ?></span><br>
 
-		        <label>Listed Price:</label>
-			        <span><?php echo ''; ?></span><br>
+		            <label>List Price:</label>
+			            <span><?php echo $list_price_formatted; ?></span><br>
 
-				        <label>Standard Discount:</label>
-					        <span><?php echo ''; ?></span><br>
+				            <label>Standard Discount:</label>
+					            <span><?php echo $discount_percent_formatted; ?></span><br>
 
-						        <label>Discount Amount:</label>
-							        <span><?php echo ''; ?></span><br>
+						            <label>Discount Amount:</label>
+							            <span><?php echo $discount_formatted; ?></span><br>
 
-								        <label>Discount Price:</label>
-									        <span><?php echo ''; ?></span><br>
-										
+								            <label>Discount Price:</label>
+									            <span><?php echo
+										    $discount_price_formatted; ?></span><br>
+
+
+						
     </main>
 </body>
 </html>
